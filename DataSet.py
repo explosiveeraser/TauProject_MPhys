cf325477285fa00b957ee0c50bd8ece1e699bfc1
explosiveeraser <aanderson021099@gmail.com>
2021-11-02 00:58:32 +0000

    Got a couple filters and columns working as well as histograms on the same graph.

---
+++ DataSet.py
@@ -0,0 +1,21 @@
+import numpy as np
+from array import array
+import ROOT
+from ROOT import gROOT
+
+class DataSet():
+
+    def __init__(self, ttree):
+        initialDFrame = ROOT.RDataFrame(ttree)
+        initialDFrame = initialDFrame.Define('Lepton_event', 'lep_n>0')
+        initialDFrame = initialDFrame.Define('Jet_event', 'jet_n>0')
+        initialDFrame = initialDFrame.Define('Photon_event', 'photon_n>0')
+        initialDFrame = initialDFrame.Define('LargeR_event', 'largeRjet_n>0')
+        initialDFrame = initialDFrame.Define('Tau_event', 'tau_n>0')
+        self.DFrame = initialDFrame
+        self.filters = np.array([])
+
+
+    def particle_Filter(self, particle):
+        filter = self.DFrame.Filter(str("good"+particle))
+        self.filters = np.append(self.filters, self.DFrame.Filter(filter))
--- Main.py
+++ Main.py
@@ -3,20 +3,38 @@ import pandas as pd
 from array import array
 import ROOT
 from ROOT import gROOT
+from DataSet import DataSet
 
+ROOT.ROOT.EnableImplicitMT()
 ROOT.gStyle.SetOptStat(0)
+
 c1 = ROOT.TCanvas("c1", "Title", 900, 700)
 c1.SetGrid()
 
 
 
-myFile = ROOT.TFile.Open("1lep1tau/Data/data_A.1lep1tau.root", "READ")
+myFile = ROOT.TFile.Open("1lep1tau/MC/mc_341123.ggH125_tautaulh.1lep1tau.root", "READ")
 myTree = myFile.mini
 
-dFrame = ROOT.RDataFrame(myTree)
+dataset = DataSet(myTree)
 
-histo = dFrame.Histo1D("tau_pt")
-histo.Draw()
+#ROOT.gInterpreter.Declare("""
+#using VecF_t = const ROOT::RVec<float>&;
+#using VecB_t = const ROOT::RVec<bool>&;
+#VecF_t FindGoodTau(VecB_t x, VecF_t tau_pt) {
+#    return tau_pt[x];
+#
+#}
+#""")
+
+#tau = dataset.DFrame.Filter("Tau_event").Histo1D('tau_pt')
+tau1 = dataset.DFrame.Define("trig_tau", "tau_pt[tau_truthMatched]").Histo1D("trig_tau")
+jet1 = dataset.DFrame.Filter("Tau_event").Histo1D("jet_pt")
+
+
+#tau.Draw()
+tau1.Draw()
+jet1.Draw("SAME")
 c1.Update()
 
 input("Press <ret> to end -> ")
