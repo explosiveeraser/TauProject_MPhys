4b8ee568bac0a3bb6834a48f72d341a683009cb9
ExplosiveEraser <adamanderson021099@outlook.com>
2022-01-20 22:39:22 +0000

    Continued building RNN trainer.

--- RNN_models/RNN_builder.py
+++ RNN_models/RNN_builder.py
@@ -1,4 +1,6 @@
+import numpy as np
 from ROOT import TMVA, TFile, TTree, TCut
+import ROOT
 from subprocess import call
 from os.path import isfile
 
@@ -7,6 +9,7 @@ from keras.utils import plot_model
 from keras.models import Model
 from keras.layers import Input
 from keras.layers import Dense
+from keras.layers import Concatenate
 from keras.layers import LSTM
 from keras.layers import Flatten
 from keras.layers.convolutional import Conv2D
@@ -15,6 +18,7 @@ from keras.layers.merge import concatenate
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.optimizers import SGD
+import tensorflow as tf
 
 TMVA.Tools.Instance()
 TMVA.PyMethodBase.PyInitialize()
@@ -31,14 +35,61 @@ Background_File = TFile.Open("../NewTTrees/background_tree_1-Prong.root")
 Signal_Tree = Signal_File.Get('signal_tree')
 Background_Tree = Background_File.Get('background_tree;6')
 
-dl_HLvars = TMVA.DataLoader('HL_vars')
-dl_Track = TMVA.DataLoader('Tracks')
-dl_Tower = TMVA.DataLoader('Towers')
+SignalDF = ROOT.RDataFrame(Signal_Tree)
+BackgroundDF = ROOT.RDataFrame(Background_Tree)
+
+print(SignalDF.GetColumnNames())
+
+#Do numpy approach for time being
+signalArray = SignalDF.AsNumpy()
+backArray = BackgroundDF.AsNumpy()
+
+SortedsignalArray = np.array([])
+SortedbackArray = np.array([])
+
+print(signalArray)
 
 
 
+
+#HL Layers
 HL_input = Input(shape=(13,))
-Track_input = Input(shape=(10,))
-Tower_input = Input(shape=(11,))
+HLdense1 = Dense(128, activation='relu')(HL_input)
+HLdense2 = Dense(128, activation='relu')(HLdense1)
+HLdense3 = Dense(16, activation='relu')(HLdense2)
+
+#Track Layers
+Track_input1 = Input(shape=(10,))
+Track_input2 = Input(shape=(10,))
+
+trackDense1 = Dense(32, activation='relu')(Track_input1)
+trackDense2 = Dense(32, activation='relu')(Track_input2)
+
+mergeTrack = Concatenate()([trackDense1, trackDense2])
+
+trackLSTM1 = LSTM(32, input_shape=(10,10))(mergeTrack)
+trackLSTM2 = LSTM(32, input_shape=(10,10))(trackLSTM1)
+
+#Tower Layers
+Tower_input1 = Input(shape=(11,))
+Tower_input2 = Input(shape=(11,))
+
+towerDense1 = Dense(32, activation='relu')(Tower_input1)
+towerDense2 = Dense(32, activation='relu')(Tower_input2)
+
+mergeTower = Concatenate()([towerDense1, towerDense2])
+
+towerLSTM1 = LSTM(24, input_shape=(6,11))(mergeTower)
+towerLSTM2 = LSTM(24, input_shape=(6,11))(towerLSTM1)
+
+#Layers Merged
+mergedLayer = Concatenate([trackLSTM2, towerLSTM2, HLdense3])
+fullDense1 = Dense(64, activation='relu')(mergedLayer)
+fullDense2 = Dense(32, activation='relu')(fullDense1)
+Output = Dense(1, activation='sigmoid')(fullDense2)
+
+RNNmodel = Model(inputs=[Track_input1, Track_input2, Tower_input1, Tower_input2, HL_input], outputs=Output)
+RNNmodel.save('tauRNN_1-prong.h5')
+RNNmodel.summary()
 
 
